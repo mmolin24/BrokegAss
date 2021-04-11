f1b78fa1f677bab722c41c04c04eb5e4bf37e539
Walter P <walterpezquera@gmail.com>
2021-04-11 00:50:19 -0400

    walter update

--- __init__.py
+++ __init__.py
@@ -1,11 +1,29 @@
+
 from flask import Flask, render_template, request, redirect, url_for
+from flask_monogoengine import MongoEngine
+#from flask_wtf import FlaskForm
+#from wtforms_fields import *
+#from wtforms.validators import DataRequired
+# ls check what's in the directory
+# cd change to directory or do cd ~ to go to home directory
+# mkdir makes a directory e.a.  mkdir hey       makes a directory named hey
+# rm -filename removes the file, if you wanna delete a directory do rm -r -directory name
+# pip install -library name
 
 
 app = Flask(__name__)
-# app.config["MONGODB_HOST"] = "mongodb://localhost:27017/project_4"
-app.config["SECRET_KEY"] = "a"
+#app.conif['SECRET_KEY']='downbadsquadgg'
+ 
+#@app.route('/')
+#def index():
 
+ #   gas_form = gasUser()
+  # return render_template('index.html',pageTitle ='Welcome to BrokegAss', form=gas_form)
 
-# db = MongoEngine(app)
-
+#@app.route('/about')
+#def about()
+ #   form = gasUser()
+  #  if form.validate_on_submit():
+   #     return render_template('about.html',pageTitle ='User Info idk')
+    #return render_template('index.html',pageTitle='BrokegAss')
 from . import routes
