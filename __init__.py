81c9cd07e5e0f56d5bb2b990ff65ce703d000364
Melvin Molina <mmolin24@terpmail.umd.edu>
2021-04-11 07:13:10 -0400

    Nearly finished with all

--- __init__.py
+++ __init__.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-
-=======
 import json
 import pyrebase
->>>>>>> 1391c4afe75eb941a086b5dd2e44cdc8e3a847b1
 from flask import Flask, render_template, request, redirect, url_for
 # from wtforms_fields import *
 #from flask_monogoengine import MongoEngine
@@ -16,33 +12,6 @@ from flask import Flask, render_template, request, redirect, url_for
 # rm -filename removes the file, if you wanna delete a directory do rm -r -directory name
 # pip install -library name
 
-
 app = Flask(__name__)
-<<<<<<< HEAD
-#app.conif['SECRET_KEY']='downbadsquadgg'
- 
-=======
-app.config["SECRET_KEY"] = "a"
-
-config = {
-    "apiKey": "AIzaSyCA2hSdjF-rRvq6xD3-s2h1xDZ1DGiMlx4",
-    "authDomain": "broke-gas.firebaseapp.com",
-    "databaseURL": "https://broke-gas-default-rtdb.firebaseio.com",
-    "projectId": "broke-gas",
-    "storageBucket": "broke-gas.appspot.com",
-    "messagingSenderId": "759807890184",
-    "appId": "1:759807890184:web:24349c11727e9e3bd252bd"
-  }
-
-firebase = pyrebase.initialize_app(config)
-
-db = firebase.database()
->>>>>>> 1391c4afe75eb941a086b5dd2e44cdc8e3a847b1
-
-#@app.route('/about')
-#def about()
- #   form = gasUser()
-  #  if form.validate_on_submit():
-   #     return render_template('about.html',pageTitle ='User Info idk')
-    #return render_template('index.html',pageTitle='BrokegAss')
+app.config['SECRET_KEY']='downbadsquadgg'
 from . import routes
