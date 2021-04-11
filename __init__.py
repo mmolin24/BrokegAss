b63b4e1a9bfbadb77c7d72309c32c570584591b9
Melvin Molina <mmolin24@terpmail.umd.edu>
2021-04-11 03:21:00 -0400

    added requirements.txt

--- __init__.py
+++ __init__.py
@@ -1,11 +1,23 @@
+import json
+import pyrebase
 from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
-# app.config["MONGODB_HOST"] = "mongodb://localhost:27017/project_4"
 app.config["SECRET_KEY"] = "a"
 
+config = {
+    "apiKey": "AIzaSyCA2hSdjF-rRvq6xD3-s2h1xDZ1DGiMlx4",
+    "authDomain": "broke-gas.firebaseapp.com",
+    "databaseURL": "https://broke-gas-default-rtdb.firebaseio.com",
+    "projectId": "broke-gas",
+    "storageBucket": "broke-gas.appspot.com",
+    "messagingSenderId": "759807890184",
+    "appId": "1:759807890184:web:24349c11727e9e3bd252bd"
+  }
 
-# db = MongoEngine(app)
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
 
 from . import routes
