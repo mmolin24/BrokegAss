98cdf6755930de0fedb5deeed061a23245a9a6bd
Juan R. Huamani <juanrafael20101@gmail.com>
2021-04-11 05:22:14 -0400

    Updated mapLink

--- googleDirections/directions.py
+++ googleDirections/directions.py
@@ -1,20 +1,23 @@
 
 
 def hyperLink(address):
+    if(address[0] == '&'):
+        return "sad"
+
     addressSplit = address.split()
     stringDirection = addressSplit[0]
-    
+
     for i in range(1,len(addressSplit)):
         if addressSplit[i] != '&':
             stringDirection += "+" + addressSplit[i]
         
     hyperLinkCreated = "https://www.google.com/maps/dir/?api=1&origin=Your+location&destination=" + stringDirection + "&travelmode=driving"
-   
+
     return hyperLinkCreated
 
 def main():
 
-    address = "13615 Georgia Ave near Hewitt Ave"
+    address = "9320 frostburg way"
     hyperLinkFound = hyperLink(address)
     print(hyperLinkFound)
 
