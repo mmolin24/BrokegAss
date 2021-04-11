5ef99fd1472125b42837183154730fca295e65a3
Juan R. Huamani <juanrafael20101@gmail.com>
2021-04-11 02:57:01 -0400

    New function

--- retrieval/gasRetrieval.py
+++ retrieval/gasRetrieval.py
@@ -3,37 +3,37 @@ import requests
 from pprint import pprint
 import zipcodes
 
-zipCode =  input("enter zip code: ")
-validZip = zipcodes.matching(zipCode)
+def nearGasStation(zipCode):
+    validZip = zipcodes.matching(zipCode)
+    stringGas = "No available gas near you"
+
+    if len(validZip) == 0:
+        print("zip does not exist!")
+    else:      
+        URL = "http://www.baltimoregasprices.com/GasPriceSearch.aspx?fuel=A&qsrch="+ zipCode
+
+        response = requests.get(URL)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        results = soup.find(id='rrlow_0')
+
+        if results is not None:
+            address_elems = results.find('dl',class_='address')
+            price_elems = results.find('div',class_='price_num')
+            update_elems = results.find('div',class_='tm')
+
+            stringGas = address_elems.text.strip() + '\n' + price_elems.text.strip() + '\n' + update_elems.text.strip()
+
+    return stringGas
+
+def main():
+    zipCodeIn =  input("enter zip code: ")
+
+    gasStation = nearGasStation(zipCodeIn)
+    print(gasStation)
 
 
-if len(validZip) == 0:
-    print("He")
-else:
-    print(validZip)
-    print("hello idiots")
-    URL = "http://www.baltimoregasprices.com/GasPriceSearch.aspx?fuel=A&qsrch="+ zipCode
 
-    response = requests.get(URL)
-    #pprint(response)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    results = soup.find(id='rrlow_0')
-
-    if results is not None:
-    
-
-        address_elems = results.find('dl',class_='address')
-        price_elems = results.find('div',class_='price_num')
-        update_elems = results.find('div',class_='tm')
-
-        print(address_elems.text.strip())
-        print(price_elems.text)
-        print(update_elems.text)
-
-
-
-
-
+main()
 
 
 
